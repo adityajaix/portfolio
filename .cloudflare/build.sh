75e654dc69fcc87f121a269d47445835f6552f82
adityajaix <adityajaix@gmail.com>
2025-11-05 00:57:13 +0530

    removed zola download script.

--- .cloudflare/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Install Zola
-curl -sL https://github.com/getzola/zola/releases/latest/download/zola-v0.17.2-x86_64-unknown-linux-gnu.tar.gz \
-  | tar xz -C /usr/local/bin
-
-# Build the site
-zola build
